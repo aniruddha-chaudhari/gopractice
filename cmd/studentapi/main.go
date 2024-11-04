1aea99026810070ed4392d3a09b4be2417090b86
aniruddha-chaudhari <140920431+aniruddha-chaudhari@users.noreply.github.com>
2024-11-04 21:28:01 +0530

    server created

--- cmd/studentapi/main.go
+++ cmd/studentapi/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"studentapi/internal/config"
+)
 
 func main() {
-	fmt.Println("Hello, World!");
+	//load the config
+	cfg := config.MustLoad()
+
+	//setup routes
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /",func(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Hello World"))	
+	})
+
+
+	//start the server
+	server := http.Server{
+		Addr: cfg.Addr,
+		Handler: router,
+	}
+
+	err := server.ListenAndServe()
+
+	if err != nil {
+		log.Fatalf("failed to start server: %v", err.Error)
+		
+	}
+
+	fmt.Println("server started on", cfg.Addr)
 }
